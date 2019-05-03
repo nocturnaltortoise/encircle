8b688e04785eb3e4d04f786eb811dd1ae844b755
Simon Turner <simonturner34@gmail.com>
2019-05-03 14:48:42 +0100

    Ensure that nodes don't overlap by using naive brute force.

--- Graph.js
+++ Graph.js
@@ -4,26 +4,16 @@ class Graph{
         this.num_nodes = num_nodes;
         this.players = players;
         this.nodes = [];
-        this.maxNeighbours = 3;
         this.canvasCtx = ctx;
         this.setUp();
         this.render();
     }
 
     setUp(){
-        let neighbourNodes = [];
+        // make the list of nodes, without neighbours or positions
         for(let i = 0; i < this.num_nodes; i++){
-            // this pointless spread on neighbourNodes is to deep copy it so the nodes
-            // keep their node lists
-            let newNode = new Node([...neighbourNodes], this.players[i % this.players.length], i);
+            let newNode = new Node([], this.players[i % this.players.length], i);
             this.nodes.push(newNode);
-
-            // neighbourNodes.push(newNode);
-            // // ensure any node has only maxNeighbours neighbours, with a rolling
-            // // list of neighbours
-            // if (neighbourNodes.length > this.maxNeighbours) {
-            //     neighbourNodes.shift();
-            // }
         }
     }
 
@@ -33,68 +23,95 @@ class Graph{
 
             let neighbourNodes = [];
             for (let k = 0; k < this.nodes.length; k++){
-                if (Math.abs(this.nodes[k].x - currentNode.x) <= 70 || Math.abs(this.nodes[k].y - currentNode.y) <= 70) {
+                if (this.distBetweenNodes(currentNode, this.nodes[k]) <= 100 && currentNode !== this.nodes[k]) {
                     neighbourNodes.push(this.nodes[k]);
                 }
             }
-            // neighbourNodes is everything within 70 of the node
 
             // make sure link is both ways
             for (let j = 0; j < neighbourNodes.length; j++) {
                 let neighbour = neighbourNodes[j];
                 neighbour.neighbours.push(currentNode);
-
-                // if this would push us over maxNeighbours, we ought not to
-                // make the link in the first place
-                // if (neighbour.neighbours.length > this.maxNeighbours) {
-                //     neighbour.neighbours.shift();
-                // }
             }
 
         }
 
     }
-
+    
+    distBetweenNodes(node, otherNode){
+      let dist = Math.sqrt(Math.pow(Math.abs(node.x - otherNode.x), 2) + Math.pow(Math.abs(node.y - otherNode.y), 2));
+      return dist;
+    }
+    
     render(){
-        let x = 70;
-        let y = 70;
+        let x = 100;
+        let y = 100;
         let rowCount = 0;
-        for (let i = 0; i < this.nodes.length; i++) {
+        let renderedNodes = [];
+        let i = 0;
+        let runCount = 0;
+        while (this.nodes.length > renderedNodes.length && runCount < 100000) {
             let currentNode = this.nodes[i];
-            this.canvasCtx.fillStyle = 'rgb(200,0,0)';
-            this.canvasCtx.moveTo(x, y);
-            this.canvasCtx.arc(x, y, 10, 0, Math.PI * 2);
+            let overlapping = false;
             currentNode.x = x;
             currentNode.y = y;
+            
+            for (let j = 0; j < renderedNodes.length; j++){
+              let renderedNode = renderedNodes[j];
+              let dist = this.distBetweenNodes(currentNode, renderedNode);
+              if (dist < 20) { // node radius assumed to equal 10
+                overlapping = true;
+              }
+            }
+            
+            if (!overlapping) {
+              
+              this.canvasCtx.fillStyle = 'rgb(200,0,0)';
+              this.canvasCtx.moveTo(x, y);
+              this.canvasCtx.arc(x, y, 10, 0, Math.PI * 2);
+              
+              x += Math.random() * 150;
+              if (i % 2 == 0) {
+                y += Math.random() * 20;
+              } else {
+                y -= Math.random() * 20;
+              }
+              
+              rowCount++;
+              if (rowCount > 4) {
+                  y += Math.random() * 150;
+                  x = Math.random() * 100;
+                  rowCount = 0;
+              };
 
-            x += 70;
-            rowCount++;
-            if (rowCount > 4) {
-                y += 70;
-                x = 70;
-                rowCount = 0;
-            };
+              this.canvasCtx.fill();
+              
+              renderedNodes.push(currentNode);
+              i++;
+            }
+            
+            runCount++;
 
-            this.canvasCtx.fill();
+          
         }
+        
+        console.log(runCount);
 
         this.calculateNeighbours();
 
-        for (let k = 0; k < this.nodes.length; k++) {
-            let currentNode = this.nodes[k];
+        for (let k = 0; k < renderedNodes.length; k++) {
+            let currentNode = renderedNodes[k];
 
             for (let l = 0; l < currentNode.neighbours.length; l++){
                 let currentNeighbour = currentNode.neighbours[l];
-                this.canvasCtx.beginPath();
-                this.canvasCtx.moveTo(currentNode.x, currentNode.y);
-                this.canvasCtx.lineTo(currentNeighbour.x, currentNeighbour.y);
-                this.canvasCtx.closePath();
-                this.canvasCtx.stroke();
+                if (renderedNodes.indexOf(currentNeighbour) !== -1) {
+                  this.canvasCtx.beginPath();
+                  this.canvasCtx.moveTo(currentNode.x, currentNode.y);
+                  this.canvasCtx.lineTo(currentNeighbour.x, currentNeighbour.y);
+                  this.canvasCtx.closePath();
+                  this.canvasCtx.stroke();
+                }
             }
         }
     }
-
-    // toString(){
-    //     return `Graph(${this.num_nodes}, ${this.players}, ${this.nodes}, ${this.maxNeighbours})`;
-    // }
 }
